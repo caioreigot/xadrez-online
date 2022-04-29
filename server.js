d026334f0927a117f01803da09c957ff3986f003
caioreigot <caiocrrodrigues2004@gmail.com>
2022-04-29 18:20:30 -0300

    Procfile adicionado

--- server.js
+++ server.js
@@ -120,6 +120,6 @@ io.on('connection', socket => {
     });
 });
 
-server.listen(port, () => {
+server.listen(process.env.PORT || port, () => {
     console.log(`Listening at ${host} on port ${port}`);
 });
